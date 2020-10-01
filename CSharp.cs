3182b079ac936e037893ab102f6eef3567b86b1d
EmirkhanDzh <emdzhaparov@edu.hse.ru>
2020-10-01 13:04:27 +0300

    Создали cs.файл
    -//-

---
+++ CSharp.cs
@@ -0,0 +1,18 @@
+CSharp.cs
+using System;
+
+namespace CSHRP
+{
+	class Program
+	{
+		static void main(string[] args)
+		{
+			Console.WriteLine("Git, hello!");
+
+		}
+		static void Meth()
+		{
+			Console.WriteLine("smthg");
+		}
+	}
+}
